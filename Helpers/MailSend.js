80be6e2c4f9ecbef19567c5bb637e8c8277730f3
upmanyu-sharma <upmanyusharma31@gmail.com>
2024-05-09 10:00:23 +0530

    minor updations

--- Helpers/MailSend.js
+++ Helpers/MailSend.js
@@ -1,22 +1,22 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  secure: true,
-  auth: {
-    user: "jobhelpnotifications@gmail.com",
-    pass: "iifrqgldrpxojlrt",
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   secure: true,
+//   auth: {
+//     user: "jobhelpnotifications@gmail.com",
+//     pass: "iifrqgldrpxojlrt",
+//   },
+// });
 
-// async function MailSend(to, subject, text, html) {
-//   const info = await transporter.sendMail({
-//     from: "jobhelpnotifications@gmail.com", // sender address
-//     to, // list of receivers
-//     subject, // Subject line
-//     text, // plain text body
-//     html, // html body
-//   });
-// }
+// // async function MailSend(to, subject, text, html) {
+// //   const info = await transporter.sendMail({
+// //     from: "jobhelpnotifications@gmail.com", // sender address
+// //     to, // list of receivers
+// //     subject, // Subject line
+// //     text, // plain text body
+// //     html, // html body
+// //   });
+// // }
 
-module.exports = { MailSend };
+// module.exports = { MailSend };
--- Routes/jobsRoute.js
+++ Routes/jobsRoute.js
@@ -57,25 +57,25 @@ router.post("/applyjob", async (req, res) => {
     currUser.appliedJobs.push(appliedJob);
     await currUser.save();
 
-    const jobPosterUser = await User.findOne({ _id: currjob.postedBy });
-    const recruiterEmail = jobPosterUser.email;
-    console.log("recruiter = " + recruiterEmail);
-    MailSend(
-      recruiterEmail,
-      `Candidate Application Received for ${currjob.title} position at ${currjob.company} on JobHelp`,
-      `Dear Sir/Mam,
+    //     const jobPosterUser = await User.findOne({ _id: currjob.postedBy });
+    //     const recruiterEmail = jobPosterUser.email;
+    //     console.log("recruiter = " + recruiterEmail);
+    //     MailSend(
+    //       recruiterEmail,
+    //       `Candidate Application Received for ${currjob.title} position at ${currjob.company} on JobHelp`,
+    //       `Dear Sir/Mam,
 
-I hope this email finds you well. I am writing to inform you that a candidate has recently applied for the ${currjob.title} position at ${currjob.company} posted on JobHelp. We are excited to connect you with potential candidates who meet your job requirements.
+    // I hope this email finds you well. I am writing to inform you that a candidate has recently applied for the ${currjob.title} position at ${currjob.company} posted on JobHelp. We are excited to connect you with potential candidates who meet your job requirements.
 
-To review the candidate's application details, please log in to your JobHelp account and navigate to the job posting for ${currjob.title} position at ${currjob.company}. There, you will find all the information provided by the candidate.
+    // To review the candidate's application details, please log in to your JobHelp account and navigate to the job posting for ${currjob.title} position at ${currjob.company}. There, you will find all the information provided by the candidate.
 
-We believe this candidate's skills and experience align well with the requirements you outlined in your job posting. We encourage you to review their application at your earliest convenience and take the necessary steps in the recruitment process.
+    // We believe this candidate's skills and experience align well with the requirements you outlined in your job posting. We encourage you to review their application at your earliest convenience and take the necessary steps in the recruitment process.
 
-Thank you for choosing JobHelp for your recruitment needs. We look forward to facilitating a successful hiring process for you.
+    // Thank you for choosing JobHelp for your recruitment needs. We look forward to facilitating a successful hiring process for you.
 
-Best regards,
-JobHelp`
-    );
+    // Best regards,
+    // JobHelp`
+    //     );
 
     res.send("Job Applied Successfully");
   } catch (error) {
